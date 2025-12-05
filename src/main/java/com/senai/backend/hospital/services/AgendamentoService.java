5f988a2c7696b3ded1178d14cc7468bd6bc8e2cb
Igorandrade2 <igorandradeferreira@gmail.com>
2025-12-05 15:55:42 -0300

    criação dos serviços

---
+++ src/main/java/com/senai/backend/hospital/services/AgendamentoService.java
@@ -0,0 +1,57 @@
+package com.senai.backend.hospital.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.senai.backend.hospital.models.Agendamento;
+import com.senai.backend.hospital.repositories.AgendamentoRepository;
+
+@Service
+public class AgendamentoService {
+
+    @Autowired
+    private AgendamentoRepository agendamentoRepository;
+
+    // salvar - POST
+    public Agendamento salvar(Agendamento agendamento) {
+        return agendamentoRepository.save(agendamento);
+    }
+
+    // buscar pelo id - GET
+    public Agendamento buscarPorId(Integer id) {
+        return agendamentoRepository.findById(id).get();
+    }
+
+    // listar todos - GET
+    public List<Agendamento> listarTodos() {
+        return agendamentoRepository.findAll();
+    }
+
+    // contar - GET
+    public long contar() {
+        return agendamentoRepository.count();
+    }
+
+    // remover pelo id - DELETE
+    public boolean removerPorId(Integer id) {
+        Agendamento agen = agendamentoRepository.findById(id).get();
+        if (agen != null) {
+            agendamentoRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+
+    // atualizar - PUT
+    public Agendamento atualizar(Integer id, Agendamento agendamento) {
+        Agendamento age = agendamentoRepository.findById(id).get();
+        if (agendamento != null) {
+            agendamento.setId(age.getId());
+            return agendamentoRepository.save(agendamento);
+        }
+        return null;
+    }
+
+}
